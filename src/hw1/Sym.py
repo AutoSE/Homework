dbe3fdf78e62dd2b0e45b4f5f2cdcd384cca2e3b
Janvi08 <janviphadtare9@gmail.com>
2023-01-18 21:23:39 -0500

    update add function

--- src/hw1/Sym.py
+++ src/hw1/Sym.py
@@ -10,7 +10,7 @@ class Sym:
     def add(self, x):
         if x != "?":
             self.n += 1
-            self.has[x] = 1 + (self.has[x] or 0)
+            self.has[x] = 1 + (self.has[x] if x in self.has else 0)
             if self.has[x] > self.most:
                 self.most,self.mode = self.has[x], x
     
