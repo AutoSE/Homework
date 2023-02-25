70ee9a2b1999a22a54a0b5793846ebae7bf64e5d
Janvi08 <janviphadtare9@gmail.com>
2023-02-25 16:18:39 -0500

    update help

--- src/hw5/cli.py
+++ src/hw5/cli.py
@@ -59,15 +59,19 @@ import re
 
 #default values
 help = "script.lua : an example script with help text and a test suite\n\n\
--d --dump   on test failure, exit with stack dump = false\n\
+-b  --bins    initial number of bins  = 16\n\
+-c  --cliffs  cliff's delta threshold = .147\n\
 -f --file with csv data = ./etc/data/auto93.csv\n\
--F  --Far     distance to \"faraway\"  = .95\n\
--g --go start-up action = data\n\
+-F  --Far     distance to \"faraway\" = .95\n\
+-g  --go      start-up action = nothing\n\
 -h --help   show help   = false\n\
+-H  --Halves  search space for clustering  = 512\n\
 -m  --min     stop clusters at N^min = .5\n\
+-M  --Max     numbers                = 512\n\
 -p  --p       distance coefficient   = 2\n\
+-r  --rest    how many of rest to sample   = 4\n\
 -s --seed   random number seed  = 937162211\n\
--S  --Sample  sampling data size     = 512\n\
+-R  --Reuse   child splits reuse a parent pole = true\n\
 "
 arg = sys.argv[1:]
 
